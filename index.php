65eb47c04ad9962e7e7329c1db1c12a7a310bd7a
LailatulKodriyah <qodriahlaila375@gmail.com>
2025-01-16 11:05:28 +0000

    add:text

--- index.php
+++ index.php
@@ -7,14 +7,26 @@
         <title>Document</title>
 </head>
 <body>
-    <h3>
+    <h1>
         <?php
-             echo "maya ganteng";
-             echo "laila dan meng comellll";
-             echo "the nyambek forever";
-                       
+         
+        define ("gaji",5000000);
+        define ("bonus",500000);
+        define ("pajak",0.12);
+
+        $jumlahproyek = 2;
+
+        $totalpendapatan = gaji + (bonus*$jumlahproyek);
+        $gajibersih = $totalpendapatan - ($totalpendapatan*pajak);
+
+        echo "gaji pendapatan yang anda peroleh 5000000";
+        echo "<br>";
+        echo "total pendapatan adalah " .$totalpendapatan;
+        echo "<br>";
+        echo "total gaji bersih adalah " .$gajibersih;
+
              ?>
-    </h3>
+    </h1>
         
 </body>
 </html>
