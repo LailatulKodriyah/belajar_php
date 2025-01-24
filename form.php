f77a46105e58efa26b73fc44adbe206f7868e024
LailatulKodriyah <qodriahlaila375@gmail.com>
2025-01-24 20:37:24 +0000

    add.text

---
+++ form.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+    <fieldset>
+        <p>
+            <label>NISN :</label>
+            <input type="text" neme="NISN" placeholder="masukan nisn">
+        </p>
+        <p>
+            <label>Nama Lengkap :</label>
+            <input type="text" name="Nama Lengkap" placeholder="masukan Nama Lengkap"> 
+        </p> 
+        <p>
+            <label>Tempat Lahir :</label>
+            <input type="text" name="Tempat Lahir" placeholder="masukan Tempat Lahir">
+        </p>
+        <p>
+            <label>Tanggal Lahir =</label>
+            <input type="date" name="Tanggal">
+        </p>
+        <p>
+            <label>Jenis Kelamin :</label>
+            <input type="radio" name="jenkel" value="Laki-Laki">Laki-Laki 
+            <input type="radio" name="jenkel" value="Perempuan">Perempuan 
+        </p>
+        <p>
+           <lanel>Agama :</label> 
+           <select name="agama">
+             <option value="budha">Budha</option>
+             <option value="hindu">Hindu</option>
+             <option value="islam">Islam</option>
+             <option value="katholik">Kristen Katholik</option>
+             <option value="protestan">Kristen Protestan</option>
+             <option value="konghucu">Konghucu</option>
+          </select> 
+        </p>
+        <p>
+            <p>Alamat :</p>
+            <textarea name="alamat"cols="30" rows="10"></textarea>
+        </p>
+        <p>
+            <label>Asal Sekolah :</label>
+            <input type="text" name="asal sekolah" placeholder="masukan asal sekolah">
+        </p>
+        <p>
+            <label>Pilih Jurusan 1:</label>
+            <select name="select">
+                <option value="MPLB">Manajemen Perkantoran dan Layanan Bisnis</option>
+                <option value="PPLG">Pengembangan Perangkat Lunak dan Gim</option>
+                <option value="AKL">Akutansi dan Keuangan Lembaga</option>
+                <option value="TJKT">Teknik Jaringan Komputer dan Telekomunikasi</option>
+                <option value="BDP">Broadcasting dan Perfilman</option>
+                <option value="PM">Pemasaran</option>
+                <option value="PH">Perhotelan</option>
+                <option value="DKV">Desain Komuniikasi Visual</option>
+                <option value="KL">Kuliner</option>
+                <option value="SP">Seni Pertunjukan</option>
+            </select>         
+        </p>
+        <p>
+            <label>Pilih Jurusan 2:</label>
+            <select name="select">
+                <option value="MPLB">Manajemen Perkantoran dan Layanan Bisnis</option>
+                <option value="PPLG">Pengembangan Perangkat Lunak dan Gim</option>
+                <option value="AKL">Akutansi dan Keuangan Lembaga</option>
+                <option value="TJKT">Teknik Jaringan Komputer dan Telekomunikasi</option>
+                <option value="BDP">Broadcasting dan Perfilman</option>
+                <option value="PM">Pemasaran</option>
+                <option value="PH">Perhotelan</option>
+                <option value="DKV">Desain Komuniikasi Visual</option>
+                <option value="KL">Kuliner</option>
+                <option value="SP">Seni Pertunjukan</option>
+            </select>         
+        </p>
+        <p>
+            <label>Ekstrakulikuler :</label>
+            <input type="checkbox" name="OSIS">Organisasi Siswa Intra Sekolah
+            <input type="checkbox" name="DA">dewan Ambalan 
+            <input type="checkbox" name="PMR">Palang Merah Remaja
+            <input type="checkbox" name="Remas">Remaja masjid
+            <input type="checkbox" name="LH">Laskar Hijau
+            <input type="checkbox" name="PA">Pecinta Alam
+            <input type="checkbox" name="Futsal">Futsal
+            <input type="checkbox" name="Basket">Basket
+            <input type="checkbox" name="Voli">Voli
+            <input type="checkbox" name="Tari">Tari
+        </p>
+        <p>
+            <input type="submit" name="submit" value="Input Data">
+        </p>
+</fieldset>
+</form>
+</body>
+</html>
